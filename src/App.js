128c4c4073c73ad608fea4064bf1cba73a46c09c
Julia <alexsandr.kotobus@gmail.com>
2024-01-08 01:09:16 +0300

    animation with react-transition-group

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import {ReactComponent as MenuIcon} from './menuIcon.svg';
+import { Transition } from 'react-transition-group';
 import './App.css';
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+  // const menuRef = useRef(null); //создаем ссылку
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        {/* вставляем компонент Transition, который будет срабатывать при изменении состояния isOpen */}
+        {/*timeout={300} - ожидание окончания анимации  */}
+        {/* и будет размонтировать меню при закрытии unmountOnExit={true}*/}
+        <Transition in={isOpen} timeout={300} unmountOnExit={true}>
+          {/* внутри Transition получаем state, добавляем state как дополнительный класс меню  */}
+        {(state)=> <nav className={`mobile-menu ${state}`}></nav>}
+        </Transition>
+       
+       {/* при клике по кнопке просто меняем состояние на противоположное setOpen(!isOpen) */}
+        <button className='menu-button' onClick={()=> setOpen(!isOpen)} >
+          <MenuIcon/>
+        </button>
       </header>
     </div>
   );
