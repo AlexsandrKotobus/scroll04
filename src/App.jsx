030ce2f0799a0e242fe56f7a5e0d09c3f2399463
Julia <alexsandr.kotobus@gmail.com>
2024-01-15 15:12:09 +0300

    Checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
-import React, {useState} from 'react'
+import React  from 'react'
 
 // import  AnimatedMenu from './components/AnimatedMenu/AnimatedMenu'
 // import DropMenu from './components/DropMenu/DropMenu';
-import { Checkbox } from './components/Checkbox/Checkbox';
+
+import FormCheck from './components/Checkbox/FormCheck';
 
 function App() {
-  const [rulesChecked, setRulesChecked] = useState(false);
-  const [mailChecked, setMailCheked] = useState(false);
+  // const [rulesChecked, setRulesChecked] = useState(false);
+  // const [mailChecked, setMailCheked] = useState(false);
   return (
     <section>
       {/* <div className="App"> */}
       {/* <AnimatedMenu/> */}
       {/* <DropMenu /> */}
-      <form className='form'>
-        <Checkbox isChecked={rulesChecked} onChange={setRulesChecked}>
-          Принимаю <a href="./doc.pdf"> правила </a> участия в акции   </Checkbox>
-        <Checkbox isChecked={mailChecked} onChange={setMailCheked}>Соглашаюсь на рассылку новостей</Checkbox>
-        <button type='submit'>Отправить</button>
-      </form>
-      
-    {/* </div> */}
+   
+       
+    <FormCheck />
 
     </section>
     
---
+++ src/components/Checkbox/FormCheck.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+
+import { Checkbox } from './Checkbox';
+
+function FormCheck() {
+  const [rulesChecked, setRulesChecked] = useState(false);
+  const [mailChecked, setMailCheked] = useState(false);
+  return (
+    <section>
+      <form className='form'>
+        <Checkbox isChecked={rulesChecked} onChange={setRulesChecked}>
+          Принимаю <a href="./doc.pdf"> правила </a> участия в акции   </Checkbox>
+        <Checkbox isChecked={mailChecked} onChange={setMailCheked}>Соглашаюсь на рассылку новостей</Checkbox>
+        <button type='submit'>Отправить</button>
+      </form>
+      
+
+    </section>
+    
+  );
+}
+
+export default FormCheck;
